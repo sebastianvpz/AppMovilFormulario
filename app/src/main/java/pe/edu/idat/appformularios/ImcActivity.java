3643912bdce3ca55399d7dd8472567ec5b00f57b
Sebastian Velarde <108041709+sebastianvpz@users.noreply.github.com>
2023-10-09 12:01:21 -0500

    first commit

---
+++ app/src/main/java/pe/edu/idat/appformularios/ImcActivity.java
@@ -0,0 +1,14 @@
+package pe.edu.idat.appformularios;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ImcActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imc);
+    }
+}
